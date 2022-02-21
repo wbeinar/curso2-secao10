2405d63acbff855ec30ef5b8868bfff475613845
wbeinar <beinar77@gmail.com>
2022-02-21 09:22:36 -0300

    Projeto iniciado

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,51 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import entities.Employee;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		List<Employee> list = new ArrayList<>();
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("How many employees wiil be registered? ");
+		int qtd = sc.nextInt();
+		
+		for(int i = 1; i <= qtd; i++) {
+			System.out.println("Employee #" + i + ":");
+			System.out.print("Id: ");
+			double id = sc.nextDouble();
+			sc.next();
+			System.out.print("Name: ");
+			String name = sc.nextLine();
+			System.out.println("Salary: ");
+			double salary = sc.nextDouble();
+			
+			list.add(new Employee(id, name, salary));
+		}
+		
+		System.out.print("Enter the Employee id that will have salary increase: ");
+		double increaseId = sc.nextDouble();
+		System.out.print("Enter the percentage(10/100): ");
+		int percent = sc.nextInt();
+		
+		//if(list.contains(increaseId));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+
 
 	}
 
---
+++ src/entities/Employee.java
@@ -0,0 +1,85 @@
+package entities;
+
+import java.util.Objects;
+
+public class Employee {
+	
+	private double id;
+	private String name;
+	private double salary;
+	
+	
+	public Employee(double id, String nome, double salario) {
+		this.id = id;
+		this.name = nome;
+		this.salary = salario;
+	}
+
+
+	public double getId() {
+		return id;
+	}
+
+
+	public void setId(double id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return name;
+	}
+
+
+	public void setNome(String nome) {
+		this.name = nome;
+	}
+
+
+	public double getSalario() {
+		return salary;
+	}
+
+
+	public void setSalario(double salario) {
+		this.salary = salario;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	
+	public double improveSalary(int	percent) {
+		return salary *= (percent/100 + 1);
+		
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		return Double.doubleToLongBits(id) == Double.doubleToLongBits(other.id);
+	}
+
+
+	@Override
+	public String toString() {
+		return "Employee \n"
+				+ "id=" + id 
+				+ "\nnome=" + name 
+				+ "\nsalario=" + salary;
+	}
+	
+	
+	
+	
+
+}
